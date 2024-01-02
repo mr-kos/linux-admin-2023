59bb95705297118a83b123060861710bc8926557
Ilya Kos <mr.ilyakos@gmail.com>
2024-01-02 10:23:17 +0300

    1st homework finally works

--- homework-1/packer/scripts/stage-1-kernel-update.sh
+++ homework-1/packer/scripts/stage-1-kernel-update.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-wget https://kernel.ubuntu.com/mainline/v6.6.6/amd64/linux-headers-6.6.6-060606-generic_6.6.6-060606.202312111032_amd64.deb
-wget https://kernel.ubuntu.com/mainline/v6.6.6/amd64/linux-headers-6.6.6-060606_6.6.6-060606.202312111032_all.deb
-wget https://kernel.ubuntu.com/mainline/v6.6.6/amd64/linux-image-unsigned-6.6.6-060606-generic_6.6.6-060606.202312111032_amd64.deb
-wget https://kernel.ubuntu.com/mainline/v6.6.6/amd64/linux-modules-6.6.6-060606-generic_6.6.6-060606.202312111032_amd64.deb
+wget https://kernel.ubuntu.com/mainline/v6.6.6/amd64/linux-headers-6.6.6-060606-generic_6.6.6-060606.202312111032_amd64.deb -o /home/vagrant/lin-head-gen-666.deb
+wget https://kernel.ubuntu.com/mainline/v6.6.6/amd64/linux-headers-6.6.6-060606_6.6.6-060606.202312111032_all.deb -o /home/vagrant/lin-head-all-666.deb
+wget https://kernel.ubuntu.com/mainline/v6.6.6/amd64/linux-image-unsigned-6.6.6-060606-generic_6.6.6-060606.202312111032_amd64.deb -o /home/vagrant/lin-image-666.deb
+wget https://kernel.ubuntu.com/mainline/v6.6.6/amd64/linux-modules-6.6.6-060606-generic_6.6.6-060606.202312111032_amd64.deb -o /home/vagrant/lin-modules-666.deb
 
-dpkg -i *.deb
+dpkg -i /home/vagrant/*.deb
 
 grub2-mkconfig -o /boot/grub2/grub.cfg
 grub2-set-default 0
-echo "Grub update done."
 shutdown -r now
-
